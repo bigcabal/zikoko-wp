a0d0bce82a40cf71a538ab9f7738f3b92e33e287
Ekerete <ekeretex@googlemail.com>
2015-06-08 14:16:20 +0100

    first commit

---
+++ local-config.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_NAME', 'techcabal');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', '127.0.0.1');
+
+define('WP_DEBUG', true);
+define('WP_CACHE', false);
