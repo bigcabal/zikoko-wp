44610b0313b634f7c62ec21e045d94fe699d47ea
Ire Aderinokun <ire@ireaderinokun.com>
2015-09-16 09:37:46 +0100

    whatsapp sharing

--- wp-content/themes/zkk/inc/conversion-prompt2.php
+++ wp-content/themes/zkk/inc/conversion-prompt2.php
@@ -52,7 +52,9 @@ global $ti_option;
 			<li class="twitter" id="share-tw-modal"> 
 				<a href="https://twitter.com/intent/tweet/?text=<?php echo $cleanTitle; ?>&url=<?php echo $cleanPermalink; ?>?utm_source=tw%26utm_campaign=zkk_share&via=zikokomag&related=zikokomag" target="_blank" class="icomoon-twitter post-share-btn"><span class="share-text">Share on</span> Twitter</a> 
 			</li><li class="whatsapp" id="share-wa"> 
-				<a href="whatsapp://send?text=<?php echo $cleanTitle; ?> - <?php echo $cleanPermalink; ?>?utm_source=wa%26utm_campaign=zkk_share" class="fawe-whatsapp" class="fawe-whatsapp post-share-btn"><i class="fa fa-whatsapp"></i> <span class="share-text">Share on</span> Whatsapp</a> 
+
+				<a href="whatsapp://send" data-text="<?php echo $rawTitle; ?> -" data-href="<?php echo $rawPermalink; ?>?utm_source=wa%26utm_campaign=zkk_share" class="fawe-whatsapp wa_btn wa_btn_s"> <i class="fa fa-whatsapp"></i> <span class="share-text">Share on</span> Whatsapp </a>
+
 			</li>
 
 			</ul>
