74d28242e5dbc6784876c65022288a1efb36c601
Ire Aderinokun <ire@ireaderinokun.com>
2015-09-22 17:35:40 +0100

    test stagin file

--- staging/wp-content/plugins/shortcodes-indep/js/frontend-toggle.js
+++
@@ -1,7 +0,0 @@
-/* Toggle Front-End */
-jQuery(document).ready(function($){
-	$('.trigger').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active').next().slideToggle('fast');
-	});
-});
--- staging/wp-content/themes/zikoko/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-add_action( 'wp_enqueue_scripts', 'tc_enqueue_styles' );
-function tc_enqueue_styles() {
-    wp_enqueue_style( 'simplemag-style', get_template_directory_uri() . '/style.css' );
-
-}
