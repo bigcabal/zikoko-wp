24ad4c1a000fbb93a59a04a17503da6bac3f1dbb
Ire Aderinokun <ire@ireaderinokun.com>
2016-01-07 10:39:34 +0100

    bugfix [d:staging]

--- functions/zkk-poll-old.php
+++ functions/zkk-poll-old.php
@@ -11,21 +11,21 @@
 $zkkpolls_table_name = 'bc_zkkpolls_answers';
 
 // will return NULL if there isn't one
-// if ( $wpdb->get_var('SHOW TABLES LIKE ' . $zkkpolls_table_name) != $zkkpolls_table_name )
-// {
-//   $sql = 'CREATE TABLE ' . $zkkpolls_table_name . '( 
-//       id INTEGER(10) UNSIGNED AUTO_INCREMENT,
-//       post_id INTEGER(10) NOT NULL default "0", 
-//       answer varchar(200) character set utf8 NOT NULL default "",
-//       responses INTEGER(10) NOT NULL default "0",
-//       PRIMARY KEY  (id) )';
+if ( $wpdb->get_var('SHOW TABLES LIKE ' . $zkkpolls_table_name) != $zkkpolls_table_name )
+{
+  $sql = 'CREATE TABLE ' . $zkkpolls_table_name . '( 
+      id INTEGER(10) UNSIGNED AUTO_INCREMENT,
+      post_id INTEGER(10) NOT NULL default "0", 
+      answer varchar(200) character set utf8 NOT NULL default "",
+      responses INTEGER(10) NOT NULL default "0",
+      PRIMARY KEY  (id) )';
   
-//   require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-//   dbDelta($sql);
+  dbDelta($sql);
   
-//   add_option('zkkpolls_database_version','1.0');
-// }
+  add_option('zkkpolls_database_version','1.0');
+}
 
 
 
--- functions/zkk-poll.php
+++ functions/zkk-poll.php
@@ -11,22 +11,22 @@
 $zkkpoll_table_name = 'bc_zkkpoll';
 
 
-// if ( $wpdb->get_var('SHOW TABLES LIKE ' . $zkkpoll_table_name) != $zkkpoll_table_name )
-// {
-// 	$sql = 'CREATE TABLE ' . $zkkpoll_table_name . '( 
-//       id INTEGER(10) UNSIGNED AUTO_INCREMENT,
-//       post_id INTEGER(10) NOT NULL default "0", 
-//       poll_key varchar(200) character set utf8 NOT NULL default "",
-//       answer varchar(200) character set utf8 NOT NULL default "",
-//       responses INTEGER(10) NOT NULL default "0",
-//       PRIMARY KEY  (id) )';
+if ( $wpdb->get_var('SHOW TABLES LIKE ' . $zkkpoll_table_name) != $zkkpoll_table_name )
+{
+	$sql = 'CREATE TABLE ' . $zkkpoll_table_name . '( 
+      id INTEGER(10) UNSIGNED AUTO_INCREMENT,
+      post_id INTEGER(10) NOT NULL default "0", 
+      poll_key varchar(200) character set utf8 NOT NULL default "",
+      answer varchar(200) character set utf8 NOT NULL default "",
+      responses INTEGER(10) NOT NULL default "0",
+      PRIMARY KEY  (id) )';
 
-// 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-// 	dbDelta($sql);
+	dbDelta($sql);
 
-// 	add_option('zkkpoll_database_version','1.0');
-// }
+	add_option('zkkpoll_database_version','1.0');
+}
 
 
 
