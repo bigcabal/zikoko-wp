cb78b2eccf0378ee8d065533f8213a158f16bd16
Ire Aderinokun <ire@ireaderinokun.com>
2016-01-28 08:14:04 +0100

    test [d:production]

--- page-belike-share.php
+++ page-belike-share.php
@@ -26,7 +26,7 @@
 <meta property="og:type" content="website">
 <meta property="og:image" content="<?php echo $meta_image; ?>">
 <meta property="og:site_name" content="Zikoko!">
-<meta property="og:url" content="<?php the_permalink(); ?>">
+<meta property="og:url" content="<?php the_permalink(); ?>?img=<?php echo $_GET['img']; ?>">
 <meta property="og:description" content="<?php echo $meta_description; ?>">
 <meta property="fb:app_id" content="593692017438309">
 
