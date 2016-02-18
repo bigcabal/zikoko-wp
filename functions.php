fe356626f7e95405d0e1cdaba821c7a322c6f37e
Ire Aderinokun <ire@ireaderinokun.com>
2016-02-18 10:17:34 +0100

    testing wpautop removal [d:staging]

--- functions.php
+++ functions.php
@@ -58,8 +58,8 @@ function custom_wpautop($content) {
     return $content;
 }
 
-remove_filter('the_content', 'wpautop');
-add_filter('the_content', 'custom_wpautop');
+//remove_filter('the_content', 'wpautop');
+//add_filter('the_content', 'custom_wpautop');
 
 
 
