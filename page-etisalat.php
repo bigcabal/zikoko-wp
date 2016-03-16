94846797aa6ae3e76aecdac983767b0eabe814a3
Ire Aderinokun <ire@ireaderinokun.com>
2016-03-16 16:42:46 +0100

    remove author/sponsor box [d:staging]

--- page-etisalat.php
+++ page-etisalat.php
@@ -36,37 +36,7 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class('main-article'); ?>>
 
 	<header class="entry-full-header">
-		<h1 class="entry-title padd-bottom"><?php the_title(); ?></h1>
-
-		<!-- Post Author -->
-		<?php 
-			$author_id = $post->post_author;
-			$author_level = get_the_author_meta( 'user_level', $author_id );
-			$author_level == '1' ? $author_title = 'Contributor' : $author_title = 'Staff Writer';
-			$author_zkk_profile = '<img alt="' . $author . '" src="' . get_the_author_meta( 'zkk_profile', $author_id ) . '" class="avatar avatar-50 photo">';
-			$author_gravatar = get_avatar($author_id, 100, 'http://zikoko.com/wp-content/uploads/2016/01/female-emoji.png', $author, ''); 
-			get_the_author_meta( 'zkk_profile', $author_id ) != '' ? $author_profile = $author_zkk_profile : $author_profile = $author_gravatar;
-
-			$author_name = get_the_author_meta( 'display_name', $author_id );
-		?>
-		<div class="post-author cf">
-		    <div class="pa__image">
-		        <?php echo $author_profile; ?>
-		    </div>
-		    <div class="pa__text">
-		        <div class="pa__text__title">
-		            <?php echo $author_title; ?>
-		        </div>
-		        <div class="pa__text__name">
-		            <?php echo $author_name; ?>
-		        </div>
-		    </div> 
-		</div>
-
-	    <!-- Post Sponsor -->
-		<?php if (  get_field( 'sponsored_post_q' ) == "yes" ) : 
-			get_template_part('inc/post', 'sponsor');
-		endif; ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header>
 
 	<div class="entry-full-content">
