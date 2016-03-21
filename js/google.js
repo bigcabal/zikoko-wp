3ebe54ee1a02b274c8573994ee03d72858d05956
Ire Aderinokun <ire@ireaderinokun.com>
2016-03-21 11:50:31 +0100

    opera datect fix, [d:production]

--- js/google.js
+++ js/google.js
@@ -15,10 +15,10 @@ ga('set', 'dimension2', author.name);
 
 // OPERA DETECTION DIMENSIONS
 if ( operaDetect.isOpera ) {
-	ga('set', 'dimension3', operaDetect.results.mode);
-	ga('set', 'dimension4', operaDetect.results.platform);
-	ga('set', 'dimension5', operaDetect.results.browser);
-	ga('set', 'dimension6', operaDetect.results.OS);
+ga('set', 'dimension3', operaDetect.results.mode);
+ga('set', 'dimension4', operaDetect.results.platform);
+ga('set', 'dimension5', operaDetect.results.browser);
+ga('set', 'dimension6', operaDetect.results.OS);
 }
 
 ga('send', 'pageview');
