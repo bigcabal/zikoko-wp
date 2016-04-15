56950e8b144027d04a4f935b4146dc8df8279e46
Ire Aderinokun <ire@ireaderinokun.com>
2016-04-15 09:53:37 +0100

    ia dev feed updates [d:staging]

--- inc/ia/article.php
+++ inc/ia/article.php
@@ -25,16 +25,11 @@
 				} ?>
 			</figure>
 
-			<details>
-				<summary>Kicker</summary>
-			</details>
-
-			<h1> v 3: <?php esc_html( the_title_rss() ); ?> </h1>
-		  	<h2> <?php the_excerpt_rss(); ?> </h2>
+			<h1><?php esc_html( the_title_rss() ); ?></h1>
+		  	<h2><?php the_excerpt_rss(); ?> </h2>
 
 			<address><?php the_author(); ?></address>
 
-			<!-- The published and last modified time stamps -->
 			<time class="op-published" dateTime="<?php echo esc_attr( get_the_time( 'c' ) ); ?>"><?php echo esc_attr( get_post_time( 'Y-m-d\TH:i:s\Z' ) ); ?></time>
 			<time class="op-modified" dateTime="<?php echo esc_attr( get_the_modified_time( 'c' ) ); ?>"><?php echo esc_attr( get_the_modified_date('Y-m-d\TH:i:s\Z') ); ?></time>
 
@@ -47,10 +42,28 @@
 		<?php do_action( 'simple_fb_after_the_cover' ); ?>
 		<?php do_action( 'simple_fb_before_the_content' ); ?>
 		<?php 
+
 			$content = get_the_content();
 			$content = str_replace("h3","h2",$content);
 			$content = str_replace("h3","h2",$content);
 			$content = str_replace("<p></p>","",$content);
+			$content = str_replace("<span></span>","",$content);
+			$content = str_replace('<div class="pcblock__media"></div>',"",$content);
+
+			$content = str_replace('<button class="ig-embed-user--follow">+ Follow</button>',"",$content);
+
+			$content = str_replace('<img class="ig-logo" src="http://zikoko.com/wp-content/uploads/2016/02/instagram-logo.png" alt="Instagram Logo">',"",$content);
+
+
+	
+
+			$content = preg_replace('#<img class="ig-embed-user--image"(.*?)>#', ' ', $content);
+
+
+			$content = preg_replace('#<span class="ig-embed-user--handle">(.*?)</span>#', ' ', $content);
+
+
+			
 
 			echo $content;
 		?>
