2247cd843c0fba7446379b36e59529799e75427e
Ire Aderinokun <ire@ireaderinokun.com>
2016-04-18 18:41:08 +0100

    instant articles cleanup

--- page-instant-articles.php
+++ page-instant-articles.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Template Name: Sponsor Homepage Feed (old)
- * Instant Articles RSS feed template
+ * Template Name: Facebook Instant Articles
+ *
+ *
  */
-// Set RSS header.
+
 header( 'Content-Type: ' . feed_content_type( 'rss-http' ) . '; charset=' . get_option( 'blog_charset' ), true );
-// Use `echo` for first line to prevent any extra characters at start of document.
 echo '<?xml version="1.0" encoding="' . esc_attr( get_option( 'blog_charset' ) ) . '"?>'; ?>
 
 <rss version="2.0"
@@ -19,43 +19,35 @@ echo '<?xml version="1.0" encoding="' . esc_attr( get_option( 'blog_charset' ) )
 >
 
 <channel>
-	<title><?php echo esc_html( wp_title() ); ?></title>
+	<title><?php echo esc_html( "Staging!" ); ?></title>
 	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 	<link><?php echo esc_url( apply_filters( 'simple_fb_home_url', home_url() ) ) ?></link>
 	<description><?php echo bloginfo( 'description' ); ?></description>
 	<lastBuildDate><?php echo esc_html( mysql2date( 'D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ) ); ?></lastBuildDate>
 	<language><?php echo bloginfo( 'language' ); ?></language>
 
-	<?php do_action( 'rss2_head' ); // Add RSS2 headers ?>
+	<?php do_action( 'rss2_head' ); ?>
 
-		<?php 
-		global $post;
-		$instant_articles = new WP_Query(
-			array(
-				'post_type'      => 'post',
-				'post_status'    => 'publish',
-				'posts_per_page' => 5
-			)
-		); ?>
-		<?php while ( $instant_articles->have_posts() ) : $instant_articles->the_post();
 
-		 		do_action( 'simple_fb_pre_render', get_the_id() ); ?>
+
+	<?php 
+	global $post;
+	$instant_articles = new WP_Query(
+		array(
+			'post_type'      => 'post',
+			'posts_per_page' => 5
+		)
+	);
+	while ( $instant_articles->have_posts() ) : $instant_articles->the_post(); ?>
 			<item>
 				<title><?php esc_html( the_title_rss() ); ?></title>
 				<link><?php the_permalink_rss(); ?></link>
 				<pubDate><?php echo esc_html( mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true ), false ) ); ?></pubDate>
-				<?php if ( function_exists( 'coauthors' ) ) : ?>
-					<?php coauthors( '</dc:creator><dc:creator>', '</dc:creator><dc:creator>', '<dc:creator>', '</dc:creator>' ); ?>
-				<?php else : ?>
-					<dc:creator><?php the_author(); ?></dc:creator>
-				<?php endif; ?>
+				<author><?php the_author(); ?></author>
 				<guid isPermaLink="false"><?php esc_html( the_guid() ); ?></guid>
 				<description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
-				<content:encoded><![CDATA[<?php include( 'inc/ia/article.php' ); ?>]]></content:encoded>
+				<content:encoded><![CDATA[<?php include( 'inc/instant-articles/article.php' ); ?>]]></content:encoded>
 			</item>
-		<?php endwhile; ?>
-	<?php endif; ?>
+	<?php endwhile; ?>
 </channel>
-
-
 </rss>
