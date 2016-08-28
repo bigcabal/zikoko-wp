239f95bf37b5698c66658f7c25f67b6f17281b50
Ire Aderinokun <ire@ireaderinokun.com>
2016-08-28 18:03:24 +0100

    changed bcm logo to cloudinary-hosted logo [d:production]

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 	<div class="bc-logo">
         <a href="http://www.bigcabal.com/" target="_blank">
-        <img src="https://res.cloudinary.com/big-cabal/image/w_100/upload/v1472403612/bc-logo-white_fcudov.png" alt="Big Cabal Logo">
+        <img src="https://res.cloudinary.com/big-cabal/image/upload/w_100/v1472403612/bc-logo-white_fcudov.png" alt="Big Cabal Logo">
         </a>
     </div>
 	
