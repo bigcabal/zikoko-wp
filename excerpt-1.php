5ac0b5e4aef157be37e87ca7e2607b3572327995
Ire Aderinokun <ire@ireaderinokun.com>
2016-08-28 15:41:48 +0100

    tmp fix

--- excerpt-1.php
+++ excerpt-1.php
@@ -12,10 +12,16 @@
 		<a href="<?php the_permalink() ?>">
 		<?php 
 		if ( get_the_post_thumbnail( $next_post->ID ) != '' ) {
-			echo get_the_post_thumbnail();
+			$thumbnail = get_the_post_thumbnail();
+			$thumbnail = str_replace(
+				'http://zikoko.com/wp-content/uploads/http://res.cloudinary.com/', 
+				'http://res.cloudinary.com/', 
+				$thumbnail);
+			echo $thumbnail;
 		} elseif( first_post_image( $next_post->ID ) ) { // Set the first image from the editor
 			echo '<img src="' . first_post_image( $next_post->ID ) . '" class="wp-post-image" />';
-		} ?>
+		} 
+		?>
 		<div class="image-cover"></div>
 		</a>
 	</div>
